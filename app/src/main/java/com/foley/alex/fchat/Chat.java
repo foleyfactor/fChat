9e33155b4bb72e6a8fc3c66f5d34ee41fe92cf6f
Alex Foley <foleyfactor98@gmail.com>
2016-06-14 17:08:54 -0400

    Commented all the code
    Now other people can read the comments and steal my ideas more easily
    :smile:

--- app/src/main/java/com/foley/alex/fchat/Chat.java
+++ app/src/main/java/com/foley/alex/fchat/Chat.java
@@ -1,5 +1,6 @@
 package com.foley.alex.fchat;
 
+//Imports
 import android.graphics.Color;
 import android.util.Log;
 import android.view.Gravity;
@@ -17,17 +18,19 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 
-/**
- * Created by Alex on 2016-05-31.
- */
+//Class declaration
 public class Chat {
 
-    private String chatId, title;
-    private static String TAG = "Chat";
-
+    //Constant class values
+    private static final String TAG = "Chat";
     private static final int MAX_BLURB_LENGTH = 40;
 
+    //Class variables
+    private String chatId, title;
+
+    //Class constructor
     public Chat(String c, String t) {
+        //Assign the class variables based on the arguments given
         this.chatId = c;
         this.title = t;
     }
@@ -36,6 +39,7 @@ public class Chat {
         //A blank constructor is required for Firebase to store/retrieve Classes.
     }
 
+    //Getters: return private class variables
     public String getChatId() {
         return this.chatId;
     }
@@ -57,59 +61,102 @@ public class Chat {
     //Method for drawing the little blurb on the screen with all of the chats on it
     //updates when a new message is sent to the chat
     public void drawChatBlurb(final LinearLayout l, final HomeActivity h, final String id) {
+        //Create a new query for the most recent message, according to timestamp, in the chat
         Query orderByTime = this.getMessageDB().child("messages").orderByChild("timestamp").limitToLast(1);
+
+        //Add a value listener for any change in value (i.e. called when messages are sent)
         orderByTime.addValueEventListener(
                 new ValueEventListener() {
                     @Override
                     public void onDataChange(DataSnapshot dataSnapshot) {
+                        //Create a layout for the blurb and set its properties
                         LinearLayout blurb = new LinearLayout(l.getContext());
                         blurb.setGravity(Gravity.CENTER_HORIZONTAL);
                         blurb.setOrientation(LinearLayout.VERTICAL);
                         blurb.setPadding(20, 20, 20, 20);
 
+                        //Create a text view with the chat's title and set its properties
                         TextView title = new TextView(l.getContext());
                         title.setText(getChat().title);
                         title.setTextSize(20);
                         title.setTextColor(Color.BLACK);
                         title.setPadding(0,5,0,15);
+
+                        //Add the title to the blurb layout
                         blurb.addView(title);
 
+                        //If the chat has children (messages)
                         if (dataSnapshot.hasChildren()) {
+                            //Get an iterable for the message
                             Iterable<DataSnapshot> message = dataSnapshot.getChildren();
+
+                            //Loop through the iterable (the one most recent message)
                             for (DataSnapshot d : message) {
+                                //Create a text view for the message and set properties
                                 TextView recentMessage = new TextView(l.getContext());
+                                recentMessage.setTextSize(14);
+                                recentMessage.setPadding(0, 5, 0, 15);
+
+                                //Get the message from the database as a Message object
                                 Message m = d.getValue(Message.class);
+
+                                //Create a string for the sender
                                 String sender;
+
+                                //Either set the sender to be "you" or the username of the sender
+                                //depending on who sent the message
                                 if (m.getUid().equals(FirebaseAuth.getInstance().getCurrentUser().getUid())) {
                                     sender = "You";
                                 } else {
                                     sender = m.getSender();
                                 }
+
+                                //If the text is longer than the blurb length, truncate it
+                                //Then, set the text of the message
                                 if (m.getText().length() + sender.length() > MAX_BLURB_LENGTH) {
                                     int messageCutoff = MAX_BLURB_LENGTH - sender.length();
                                     recentMessage.setText(sender + ":  " + m.getText().substring(0, messageCutoff + 1) + "...");
                                 } else {
                                     recentMessage.setText(sender + ":  " + m.getText());
                                 }
-                                recentMessage.setTextSize(14);
-                                recentMessage.setPadding(0, 5, 0, 15);
+
+                                //Add the recent message layout to the blurb
                                 blurb.addView(recentMessage);
                             }
                         } else {
+                            //Note: this should never be called because messages are sent to the chat
+                            //on creation announcing creation and joining of the chat. Regardless,
+                            //if messages somehow get deleted from the database this will be called
+
+                            //Create a new text view for the text and set properties
                             TextView newChat = new TextView(l.getContext());
-                            newChat.setText("New chat...");
                             newChat.setTextSize(14);
                             newChat.setPadding(0,5,0,15);
+
+                            //Set the blurb to say that this is a new chat
+                            newChat.setText("New chat...");
+
+                            //Add the new chat text to the blurb
                             blurb.addView(newChat);
                         }
+
+                        //Create a layout for the divider line between chats in the list
                         LinearLayout divider = new LinearLayout(l.getContext());
-                        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, 2);
-                        divider.setLayoutParams(params);
                         divider.setBackgroundColor(Color.BLACK);
 
+                        //Create layout parameters specifying the width and height of the divider
+                        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, 2);
+                        divider.setLayoutParams(params);
+
+                        //Add the divider to the blurb
                         blurb.addView(divider);
+
+                        //Give the blurb a tag (id) corresponding to the chatId so that we can
+                        //remove it if we have a newer message to display
                         blurb.setTag(chatId);
 
+                        //Add an click listener to the text view that transitions to the chat
+                        //that's clicked on
                         blurb.setOnClickListener(new View.OnClickListener() {
                             @Override
                             public void onClick(View v) {
@@ -117,10 +164,16 @@ public class Chat {
                             }
                         });
 
+                        //Get the old blurb with the same chatId (for updating the most recent message)
                         LinearLayout oldBlurb = (LinearLayout) l.findViewWithTag(chatId);
+
+                        //If there is a view with that tag, remove it and add this chat to the top of
+                        //the list
                         if (oldBlurb != null) {
                             l.removeView(oldBlurb);
                             l.addView(blurb, 0);
+
+                        //Otherwise, just add this blurb to the list
                         } else {
                             l.addView(blurb);
                         }
@@ -128,6 +181,7 @@ public class Chat {
 
                     @Override
                     public void onCancelled(DatabaseError databaseError) {
+                        //Log the error so it can be fixed
                         Log.e(TAG, "DrawChatBlurb: error");
                     }
                 }
--- app/src/main/java/com/foley/alex/fchat/Message.java
+++ app/src/main/java/com/foley/alex/fchat/Message.java
@@ -1,5 +1,6 @@
 package com.foley.alex.fchat;
 
+//Imports
 import android.app.ProgressDialog;
 import android.content.Context;
 import android.graphics.Color;
@@ -15,43 +16,48 @@ import com.google.firebase.auth.FirebaseAuth;
 import java.text.DateFormat;
 import java.util.Date;
 
-/**
- * Created by Alex on 2016-05-31.
- */
+//Class declaration
 public class Message {
+
+    //Class variables
     private String text, sender, uid;
     private long timestamp;
-    private static String TAG = "Message";
 
     public Message() {
         // Default constructor required for calls to DataSnapshot.getValue(Message.class)
     }
 
+    //Class constructor
     public Message(String t, String s, Long d, String u) {
+        //Assign the object's variables based on the parameters
         this.text = t;
         this.sender = s;
         this.timestamp = d;
         this.uid = u;
     }
 
-    public String getText() {
-        return this.text;
-    }
+    //Getters: return the private class variables
+    public String getText() { return this.text; }
 
-    public String getSender() {
-        return this.sender;
-    }
+    public String getSender() { return this.sender; }
 
     public String getUid() { return this.uid; }
 
+    public long getTimestamp() { return timestamp; }
+
     //Returns the layout corresponding to the message, with spacing and background
     public LinearLayout formatMessage(Context c) {
+        //Check if the sender is the same as the current user
         boolean sentByUser = this.uid.equals(FirebaseAuth.getInstance().getCurrentUser().getUid());
+        boolean sentBySystem = this.uid.equals("@system");
 
+        //Create a linear layout that will contain the message, a layout for the message and a view
+        //for optional right alignment
         LinearLayout outerLayout = new LinearLayout(c);
         LinearLayout messageLayout = new LinearLayout(c);
         View alignRight = new View(c);
 
+        //Create layout parameters specifying the layouts' widths, heights, and weights
         LinearLayout.LayoutParams outerParams =
                 new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
         LinearLayout.LayoutParams innerParams =
@@ -60,45 +66,66 @@ public class Message {
         LinearLayout.LayoutParams rightParams =
                 new LinearLayout.LayoutParams(0, 0, 1);
 
-        alignRight.setLayoutParams(rightParams);
+        //Set the orientation of the message layout to vertical and the outer layout to horizontal
         outerLayout.setOrientation(LinearLayout.HORIZONTAL);
         messageLayout.setOrientation(LinearLayout.VERTICAL);
 
+        //Set the layout parameters for the layouts and view
         outerLayout.setLayoutParams(outerParams);
         messageLayout.setLayoutParams(innerParams);
+        alignRight.setLayoutParams(rightParams);
 
+        //Create a text view for the sender's info
         TextView senderText = new TextView(c);
+
+        //Set the sender to be "You" if the user sent it, and the username of the sender if someone
+        //else sent it
         if (sentByUser) {
             senderText.setText("You");
         } else {
             senderText.setText(this.sender);
         }
+
+        //Set the text to be a dark gray
         senderText.setTextColor(Color.DKGRAY);
 
+        //Create a text view for the message, set its text and text's color
         TextView messageText = new TextView(c);
         messageText.setText(this.text);
         messageText.setTextColor(Color.BLACK);
 
+        //Create a text view for the timestamp, set its text and the text's color
         TextView timestampText = new TextView(c);
         timestampText.setText(DateFormat.getDateTimeInstance().format(new Date(this.timestamp)));
         timestampText.setTextColor(Color.DKGRAY);
 
+        //Add all of the views to the message's layout
         messageLayout.addView(senderText);
         messageLayout.addView(messageText);
         messageLayout.addView(timestampText);
 
+        //Check if the message was sent by the user
         if (sentByUser) {
+            //If so, align everything right and set the background to be the user message background
             outerLayout.addView(alignRight);
+
             senderText.setTextAlignment(View.TEXT_ALIGNMENT_TEXT_END);
             timestampText.setTextAlignment(View.TEXT_ALIGNMENT_TEXT_END);
             messageText.setTextAlignment(View.TEXT_ALIGNMENT_TEXT_END);
+
             messageLayout.setBackgroundResource(R.drawable.user_message);
+        } else if (sentBySystem) {
+            //Set the background to be that of a system message
+            messageLayout.setBackgroundResource(R.drawable.system_message);
         } else {
+            //Otherwise, leave it left aligned and set the background to be that of a regular message
             messageLayout.setBackgroundResource(R.drawable.other_message);
         }
 
+        //Add message view to the outer layout
         outerLayout.addView(messageLayout);
 
+        //Return the outer layout
         return outerLayout;
     }
 }
